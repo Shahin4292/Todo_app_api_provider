fd7b65b2faec498ba5818df17bf282ceea53e4ec
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-07-12 00:43:23 +0600

    TodoEdit screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_apps/src/view/todo_create.dart';
+import 'package:todo_apps/src/view/todo_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const TodoCreate(),
     );
   }
 }
