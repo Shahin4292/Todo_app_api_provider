6970c1870fa5e55e52ed07bf0700ca78243ab055
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-07-12 00:43:17 +0600

    TodoEdit screen done

---
+++ lib/src/view/todo_list.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text("Todo List"),
+      ),
+      body: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 3,
+              itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                tileColor: Colors.white,
+                title: const Text("title"),
+                subtitle: const Text("subtitle"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem(
+                        value: "edit",
+                        child: Text("Edit"),
+                    ),
+                    const PopupMenuItem(
+                      value: "delete",
+                        child: Text("delete"))
+                  ],
+                  onSelected: (String value) {
+                    if(value == "edit"){}
+                    if(value == "delete"){}
+                  },
+
+                )
+              ),
+            );
+          }
+          )
+        ],
+      ),
+    );
+  }
+}
