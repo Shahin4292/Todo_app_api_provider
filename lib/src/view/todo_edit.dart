8d1f9f17e7c4edc96b6cc7f4bc0bcc59f0cafe5a
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-07-12 22:30:40 +0600

    todoEdit done

---
+++ lib/src/view/todo_edit.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class TodoEdit extends StatefulWidget {
+  const TodoEdit({super.key});
+
+  @override
+  State<TodoEdit> createState() => _TodoEditState();
+}
+
+class _TodoEditState extends State<TodoEdit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text("Todo List"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: 3,
+              itemBuilder: (context, index){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    tileColor: Colors.white,
+                    title: const Text("title"),
+                    subtitle: const Text("subtitle"),
+                    trailing: PopupMenuButton(
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                        const PopupMenuItem(
+                          value: "edit",
+                          child: Text("Edit"),
+                        ),
+                        const PopupMenuItem(
+                            value: "delete",
+                            child: Text("delete"))
+                      ],
+                      onSelected: (String value) {
+                        if(value == "edit"){}
+                        if(value == "delete"){}
+                      },
+                    ),
+                  ),
+                );
+              }
+          )
+        ],
+      ),
+    );
+  }
+}
