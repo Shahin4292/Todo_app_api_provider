e0c9d9be8c3173d3cb322876c77c83b98f105da3
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-07-12 21:13:11 +0600

    TodoEdit screen done

--- lib/src/view/todo_list.dart
+++ lib/src/view/todo_list.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
